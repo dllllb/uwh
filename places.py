a2d21d7d23a9cb6131ae20fd73ae70f3fdc1ac44
Dmitri Babaev <dmitri.babaev@gmail.com>
2022-12-29 14:47:56 +0200

    place image with link

--- places.py
+++ places.py
@@ -9,7 +9,20 @@ places = pd.read_xml('uwh.xml', xpath='.//row', parser='etree')
 for i, r in places.iterrows():
     color = 'green' if r.category == 'Natural' else 'blue'
 
-    popup = folium.Popup(r.short_description, max_width=512)
+    popup = f"""
+    <div style="display: table">
+        <div style="display: flex; padding: 4px; float: left; width: 15%">
+            <a href="{r.http_url}">
+                <img style="max-height: 100%; max-width: 100%; border: 5px" align="left" src="{r.image_url}" />
+            </a>
+        </div>
+        <div style="display: flex; float: left; width: 85%">
+            {r.short_description}
+        </p>
+    </div>
+    """
+
+    popup = folium.Popup(popup, max_width=512)
     folium.CircleMarker(
         location=(r.latitude, r.longitude),
         radius=1,
